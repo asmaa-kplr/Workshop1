506169b19547ab395e3e49d4a64245b1696308a6
asmaa-kplr <asmaa.tounsi@kplr.fr>
2023-06-14 09:34:01 +0000

    Ajout de la fonction de division

--- calculatrice.py
+++ calculatrice.py
@@ -7,3 +7,8 @@ def soustraction(a, b):
 def multiplication(a, b):
     return a * b
 
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erreur : Division par zéro"
